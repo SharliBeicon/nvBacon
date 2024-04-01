db8b142f4232b68247324a7f5d9355af1f7b3ce4
Carlos Tocino <carlostocinocubelo@gmail.com>
2024-04-01 10:31:40 +0200

    My NvChad Config

---
+++ lua/plugins/init.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre',
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier",
+        "typescript-language-server",
+        "astro-language-server",
+        "csharp-language-server", "csharpier",
+        "tailwindcss-language-server"
+  		},
+  	},
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css", "c_sharp",
+        "cpp", "gitignore", "go",
+        "json", "rust", "sql",
+        "tsx", "typescript", "yaml"
+      },
+  	},
+  },
+}
