8a3328dbc8d26b216bfec11c198558146f78ad97
Carlos Tocino <carlostocinocubelo@gmail.com>
2024-04-01 14:04:10 +0200

    added dashboard to nvchad

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,11 +6,35 @@ local M = {}
 
 M.ui = {
 	theme = "catppuccin",
+  nvdash = {
+    load_on_startup = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    header = {
+      '          ▀████▀▄▄              ▄█ ',
+      '            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ',
+      '    ▄        █          ▀▀▀▀▄  ▄▀  ',
+      '   ▄▀ ▀▄      ▀▄              ▀▄▀  ',
+      '  ▄▀    █     █▀   ▄█▀▄      ▄█    ',
+      '  ▀▄     ▀▄  █     ▀██▀     ██▄█   ',
+      '   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ',
+      '    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ',
+      '   █   █  █      ▄▄           ▄▀   ',
+      '                                   ',
+    },
+
+    footer = {
+      '🥓 Charlie Bacon\'s IDE'
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 }
 
 return M
