d8f440c90e5ad9844d8721cb03336d896b65a6a6
Carlos Tocino <carlostocinocubelo@gmail.com>
2024-04-03 12:43:40 +0200

    some more useful mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("n", "<leader><Up>", "ddkP")
-map("n", "<leader><Down>", "ddp")
+map("n", "<leader>u", "ddkP", { desc = "Move line Up" })
+map("n", "<leader>d", "ddp", { desc = "Move line Down" })
+map("n", "<leader>X", ":%bd<CR><leader>e", { desc = "Close all Buffers" })
+map("n", "j", "jzz", {desc = "Cursor Down"})
+map("n", "k", "kzz", {desc = "Cursor Up"})
+map("n", "<leader>O", "O<ESC>", {desc = "Create Line Down without Insert Mode"})
+map("n", "<leader>o", "o<ESC>", {desc = "Create Line Up without Insert Mode"})
 vim.wo.number = true
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
