4d91338d38daa267abe5a4c1e415ccc1d4389849
Charlie Bacon <carlos.tocino@payretailers.com>
2024-06-25 16:28:50 +0200

    clangd added

--- init.lua
+++ init.lua
@@ -583,6 +583,7 @@ require('lazy').setup({
         gopls = {},
         rust_analyzer = {},
         tailwindcss = {},
+        clangd = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
