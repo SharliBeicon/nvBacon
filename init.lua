8d73880d0b227b05950fb7b3292343dcd42aea7c
Charlie Bacon <ct@databeacon.aero>
2024-08-14 14:59:21 +0200

    new plugins

--- init.lua
+++ init.lua
@@ -589,6 +589,7 @@ require('lazy').setup({
         rust_analyzer = {},
         tailwindcss = {},
         clangd = {},
+        pylsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
