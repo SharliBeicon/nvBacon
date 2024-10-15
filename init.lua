ab2995074bb1eef80b710394c2421267e4af9284
Carlos Tocino <ct@databeacon.aero>
2024-10-15 16:55:25 +0200

    new stuff

--- init.lua
+++ init.lua
@@ -626,6 +626,7 @@ require('lazy').setup({
         sqlls = {},
         zls = {},
         csharp_ls = {},
+        hls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
